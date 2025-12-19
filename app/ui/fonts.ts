cac1d9055a40a1ea3f56c8319f28b53c3ca9878c
Mohammed-Khaledx <mohammedkhaledx1@gmail.com>
2025-12-19 20:53:25 +0200

    remove unused font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,5 @@ import { Inter ,Kufam ,Lusitana} from 'next/font/google';
 
  
 export const inter = Inter({ subsets: ['latin'] });
-export const kufam = Kufam({weight: '700'});
+// export const kufam = Kufam({weight: '700'});
 export const lusitana = Lusitana({weight: ['700'] , subsets:['latin']})
